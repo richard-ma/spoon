249120055056706dca1d9a8052dea21120c79ee4
Richard Ma <richard.ma.19850509@gmail.com>
2021-03-18 11:19:39 +0800

    use mongodb

--- spoon/spoon/pipelines.py
+++ spoon/spoon/pipelines.py
@@ -7,10 +7,25 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import pymongo
+from scrapy.utils.project import get_project_settings
+from scrapy.exceptions import DropItem
+
 
 class SinaTopSummaryPipeline:
+    def __init__(self):
+        self.settings = get_project_settings()
+        connection = pymongo.MongoClient(
+            self.settings['MONGODB_SERVER'],
+            self.settings['MONGODB_PORT']
+        )
+        db = connection[self.settings['MONGODB_DB']]
+        self.collection = db[self.settings['MONGODB_COLLECTION']]
+
     def process_item(self, item, spider):
-        print(item)
+        self.collection.insert(dict(item))
+        # log.msg("Question added to MongoDB database!",
+        #         level=log.DEBUG, spider=spider)
         return item
 
 
