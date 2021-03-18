cbe25cb21b86c482d86dd238ccd467d8f1462974
Richard Ma <richard.ma.19850509@gmail.com>
2021-03-18 13:02:10 +0800

    same last_update time

--- spoon/spoon/spiders/sinaTopSummary.py
+++ spoon/spoon/spiders/sinaTopSummary.py
@@ -10,11 +10,12 @@ class SinatopsummarySpider(scrapy.Spider):
     start_urls = ['https://s.weibo.com/top/summary/']
 
     def parse(self, response):
+        last_update = datetime.now()
         tr_list = response.xpath('//tr')
         for tr in tr_list:
             item = SinaTopSummaryItem()
             item['ranking'] = tr.xpath('./td[contains(@class, "ranktop")]/text()').extract_first()
             item['summary'] = tr.xpath('./td[@class="td-02"]/a/text()').extract_first()
-            item['last_update'] = datetime.now()
+            item['last_update'] = last_update
 
             yield item
